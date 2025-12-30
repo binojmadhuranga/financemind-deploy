ef75be99a127026ae80c041a10916f57365b7eb4
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-12-30 15:13:40 +0530

    Validate PORT environment variable and set default value

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import app from "./app";
 import sequelize from "./config/database";
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT);
+
+if (!PORT) {
+  throw new Error("PORT is not defined");
+}
+
 
 (async () => {
   try {
